dffc56b8de7850e76031e8966e935ca41cbc5db1
Your Name <the-lan.nguyen6@etu.univ-lorraine.fr>
2025-11-03 23:48:52 +0100

    jsp trop je demande a philemon

--- ProjetWeb2/recherche.php
+++ ProjetWeb2/recherche.php
@@ -1,8 +1,8 @@
 <?php
 if (isset($_POST['texte'])) {
     $texte = $_POST['texte'];
-    $mots = preg_split('/\s+/', trim($texte));
+    //$mots = preg_split('/\s+/', trim($texte));
     header('Content-Type: application/json');
-    echo json_encode($mots);
+    echo json_encode($texte);
 }
 ?>
